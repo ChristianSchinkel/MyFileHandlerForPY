2adea95473dc535074fe5970a9927e44f0da7b18
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-03-06 21:50:55 +0100

    Refactor main function to utilize start_end module for file operations and welcome message display

--- __main__.py
+++ __main__.py
@@ -1,16 +1,12 @@
 """The main entry point for the application."""
-from InputController.inputController import InputController  # type: ignore
-
-from file_manager_py_package.memory_manager import MemoryManager
-# csv_manager, file_manager
-WELCOME_MESSAGE = "Welcome to the File Manager for Python!"
+import utils.start_end as se
 
 
 def main():
     """The main function."""
-    display_welcome_message()
+    se.display_welcome_message()
     while True:
-        import_files_and_clear_data(option="import")
+        se.import_files_and_clear_data(option="import")
         #
         #
         #
@@ -25,7 +21,7 @@ def main():
         #
         #
         #
-        import_files_and_clear_data(option="clear")
+        se.import_files_and_clear_data(option="clear")
         # Break if exit is typed, otherwise continue the loop
         exit_response = input("Type 'exit' to quit or press \
                               Enter to continue: ")
@@ -34,34 +30,6 @@ def main():
             break
 
         print("Continuing the application...")
-
-
-def import_files_and_clear_data(option: str):
-    """Handle importing files and clearing data based on option."""
-    ic = InputController()
-    mm = MemoryManager()
-    if option == "import":
-        response = ic.format_bool("Do you want to import files \
-                   into the application? (yes/no): ")
-        if response:
-            print("You chose to import files.")
-            mm.copy_files_from_directory()
-        else:
-            print("You chose not to import files.")
-
-    elif option == "clear":
-        response = ic.format_bool("Do you want to clear \
-                        the application data? (yes/no): ")
-        if response:
-            print("You chose to clear the application data.")
-            mm.clear_app_data()
-        else:
-            print("You chose not to clear the application data.")
-
-
-def display_welcome_message():
-    """Display the welcome message."""
-    print(WELCOME_MESSAGE)
 
 
 if __name__ == "__main__":
---
+++ utils/start_end.py
@@ -0,0 +1,34 @@
+"""Start and end functions for the application."""
+from InputController.inputController import InputController  # type: ignore
+
+from file_manager_py_package.memory_manager import MemoryManager
+# csv_manager, file_manager
+WELCOME_MESSAGE = "Welcome to the File Manager for Python!"
+
+
+def import_files_and_clear_data(option: str):
+    """Handle importing files and clearing data based on option."""
+    ic = InputController()
+    mm = MemoryManager()
+    if option == "import":
+        response = ic.format_bool("Do you want to import files \
+                   into the application? (yes/no): ")
+        if response:
+            print("You chose to import files.")
+            mm.copy_files_from_directory()
+        else:
+            print("You chose not to import files.")
+
+    elif option == "clear":
+        response = ic.format_bool("Do you want to clear \
+                        the application data? (yes/no): ")
+        if response:
+            print("You chose to clear the application data.")
+            mm.clear_app_data()
+        else:
+            print("You chose not to clear the application data.")
+
+
+def display_welcome_message():
+    """Display the welcome message."""
+    print(WELCOME_MESSAGE)
