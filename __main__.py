ab6b2effc22aa2c61a385db0168d61d5ee237ca8
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-03-06 21:41:11 +0100

    Refactor main function and enhance import/clear data functionality in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
 """The main entry point for the application."""
-import os
-from time import sleep
-import InputController
+from InputController.inputController import InputController  # type: ignore
 
 from file_manager_py_package.memory_manager import MemoryManager
 # csv_manager, file_manager
@@ -10,22 +8,60 @@ WELCOME_MESSAGE = "Welcome to the File Manager for Python!"
 
 def main():
     """The main function."""
-    print(WELCOME_MESSAGE)
-    # fh = file_manager.FileManager("app_data/test.txt")
-    # ch = csv_manager.CSVManager("app_data/test.csv")
-    mm = MemoryManager()
-    print(mm.directory_manager)
-    mm.copy_files_from_directory()
-    # Wait for 5 seconds before proceeding
-    print("Waiting for 5 seconds...")
-    sleep(2)
+    display_welcome_message()
+    while True:
+        import_files_and_clear_data(option="import")
+        #
+        #
+        #
+        # Here you can add more functionality or
+        # options for the user to interact with the application.
+        #
+        #
+        #
+        #
+        # Here you can add more functionality or
+        # options for the user to interact with the application.
+        #
+        #
+        #
+        import_files_and_clear_data(option="clear")
+        # Break if exit is typed, otherwise continue the loop
+        exit_response = input("Type 'exit' to quit or press \
+                              Enter to continue: ")
+        if exit_response.lower() == 'exit':
+            print("Exiting the application. Goodbye!")
+            break
 
-    # Clear the console
-    os.system('cls' if os.name == 'nt' else 'clear')
-    mm.clear_app_data()
-    # fh.write("This is a test file.")
-    # ch.write_csv([{"name": "John", "age": 30},
-    #               {"name": "Jane", "age": 25}])
+        print("Continuing the application...")
+
+
+def import_files_and_clear_data(option: str):
+    """Handle importing files and clearing data based on option."""
+    ic = InputController()
+    mm = MemoryManager()
+    if option == "import":
+        response = ic.format_bool("Do you want to import files \
+                   into the application? (yes/no): ")
+        if response:
+            print("You chose to import files.")
+            mm.copy_files_from_directory()
+        else:
+            print("You chose not to import files.")
+
+    elif option == "clear":
+        response = ic.format_bool("Do you want to clear \
+                        the application data? (yes/no): ")
+        if response:
+            print("You chose to clear the application data.")
+            mm.clear_app_data()
+        else:
+            print("You chose not to clear the application data.")
+
+
+def display_welcome_message():
+    """Display the welcome message."""
+    print(WELCOME_MESSAGE)
 
 
 if __name__ == "__main__":
