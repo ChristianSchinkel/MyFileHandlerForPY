a8f4b3887d58ce5ebfeec2d185dd0853337a628f
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-03-13 23:23:32 +0100

    Remove unused modules and refactor main application entry point to a simple greeting

--- __main__.py
+++ __main__.py
@@ -1,35 +1,9 @@
 """The main entry point for the application."""
-import utils.start_end as se
 
 
-def main():
+def main() -> None:
     """The main function."""
-    se.display_welcome_message()
-    while True:
-        se.import_files_and_clear_data(option="import")
-        #
-        #
-        #
-        # Here you can add more functionality or
-        # options for the user to interact with the application.
-        #
-        #
-        #
-        #
-        # Here you can add more functionality or
-        # options for the user to interact with the application.
-        #
-        #
-        #
-        se.import_files_and_clear_data(option="clear")
-        # Break if exit is typed, otherwise continue the loop
-        exit_response = input("Type 'exit' to quit or "
-                              "press Enter to continue: ")
-        if exit_response.lower() == 'exit' or exit_response.lower() == 'quit':
-            print("Exiting the application. Goodbye!")
-            break
-
-        print("Continuing the application...")
+    print("Hello, World!")
 
 
 if __name__ == "__main__":
