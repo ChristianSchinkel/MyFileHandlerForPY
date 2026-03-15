7a530bae0bc56b9e7b53708c3b50844f0219e7b3
Christian Schinkel <107631710+ChristianSchinkel@users.noreply.github.com>
2026-03-15 21:13:42 +0100

    Refactor main function to improve database table creation and data insertion, and enhance data retrieval with sample data display

--- data_manager/database_managment_system.py
+++ data_manager/database_managment_system.py
@@ -23,26 +23,68 @@ def main() -> None:
     dbms = DatabaseManagementSystem()
     # Example usage of the DatabaseManagementSystem class
     print("This is the Database Management System Module.")
-    connection = sqlite3.connect(dbms.db_name)
+    con = sqlite3.connect(dbms.db_name)
     # Create a list of table containing strings
     table_names = []
     table_name = input("Enter the name of the table to create: ")
     table_names.append(table_name)
+    # Create Cursor object to execute SQL commands
+    cur = con.cursor()
     # Create a table in the database
-    connection.execute(f'''
+    cur.execute(f'''
 CREATE TABLE IF NOT EXISTS {table_name.lower()} (
     id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL
+    title TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    score REAL NOT NULL
 )
                        ''')
-    # Write the table names to a file
-    connection.commit()
-    # Close the connection to the database
-    connection.close()
-    # Create a sample table
+    # Check if the table was created successfully
+    res = cur.execute('''SELECT name FROM sqlite_master''')
+    res.fetchone()
+    # Return Boolean if a table is not existing.
+    res = cur.execute("""SELECT name FROM sqlite_master WHERE name='spam'""")
+    print(res.fetchone() is None)
+    # Insert data into the table
+    cur.execute(f"""
+        INSERT INTO {table_name.lower()} VALUES
+            ('Monty Python and the Holy Grail', 1975, 8.2),
+            ('And Now for Something Completely Different', 1971, 7.5)
+                """)
+    con.commit()
+    # Query the table to retrieve data
+    res = cur.execute("SELECT score FROM movie")
+    res.fetchall()
 
     # Insert sample data into the table
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0),
+    ]
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+    # Notice that ? placeholders are used to bind data to the query.
+    # Always use placeholders instead of string formatting
+    # to bind Python values to SQL statements,
+    # to avoid SQL injection attacks (see How to use placeholders
+    # to bind values in SQL queries for more details).
+    con.commit()  # Remember to commit the transaction after executing INSERT.
+    # Print the data in the table
+    for row in cur.execute("""
+                           SELECT year, title FROM movie ORDER BY year
+                           """):
+        print(row)
+
+    # Close the connection to the database
+    con.close()
+    # Create a new connection to the database and query the data
+    new_con = sqlite3.connect("tutorial.db")
+    new_cur = new_con.cursor()
+    res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
+    title, year = res.fetchone()
+    print(f"""The highest scoring Monty Python movie is {title!r},
+          released in {year}""")
+    new_con.close()
 
 
 if __name__ == "__main__":
